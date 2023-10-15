6ee146edfb4c67c688841310a7bb76a8cdfb4a4b
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2023-10-15 09:01:18 +0700

    Add files via upload

---
+++ Lab_1_flowchart/E6quadraticEquation.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main() {
+
+    float a, b, c, delta, root1, root2;
+
+    cout << "a: ";
+    cin >> a;
+    cout << "b: ";
+    cin >> b;
+    cout << "c: ";
+    cin >> c;
+
+    delta = (b*b) - (4*a*c);
+
+    if (delta < 0) {
+        cout << "There are no real roots.";
+    }
+    else if (delta == 0) {
+        cout << "Root = " << -b / (2*a);
+    }
+    else {
+        root1 = (-b + sqrt(delta)) / (2*a);
+        root2 = (-b - sqrt(delta)) / (2*a);
+
+        cout << "The first root = " << root1 << endl
+        << "The second root = " << root2;
+    }
+
+    return 0;
+}
