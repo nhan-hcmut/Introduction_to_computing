0d4d6986bc4546326160c3996a21d3aaed7042d5
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2023-10-16 12:08:27 +0700

    Add files via upload

---
+++ Lab_2/E11oddEven.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int num;
+
+    cout << "Enter an integer: ";
+    cin >> num;
+
+    (num%2 != 0) ? cout << "Ddd" : cout << "Even"; // I must NOT use if statement
+
+    return 0;
+}
