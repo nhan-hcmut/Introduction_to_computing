85b6d27762dca623c2e2f10348725e6105eff539
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2023-10-23 21:35:29 +0700

    Add files via upload

---
+++ Lab_3_Selection/E10leapYear.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int year;
+
+    cout << "Year: ";
+    cin >> year;
+    
+    if (year%400 == 0) {
+        cout << "Leap year";
+    }
+    else if ( (year%4 == 0) && (year%100 != 0) ) {
+        cout << "Leap year";
+    }
+    else {
+        cout << "Not leap year";
+    }
+
+    return 0;
+}
