ff62244db1d164a33a2e898d4a0dff90ac61d102
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2023-11-10 21:39:46 +0700

    Add files via upload

---
+++ Lab_4_Loop/E13wages.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+
+    int i;
+
+    double hourly_rates[5] = {9.5 , 6.4 , 12.5 , 5.5 , 10.5}, working_hours[5], wages[5];
+
+    for (i=0; i<5; i++) {
+
+        cout << "Working hour " << i+1 << ": ";
+        cin >> working_hours[i];
+        wages[i] = hourly_rates[i] * working_hours[i];
+    }
+    
+    for (i=0; i<5; i++) {
+        cout << i+1 << "\t" << "Hour rate = " << hourly_rates[i] << ", " << "working hour = " << working_hours[i] << " , wage = " << wages[i] << endl << endl;
+    }
+    return 0;
+}
