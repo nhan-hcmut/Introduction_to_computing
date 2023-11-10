e91ea9046d11ac54814c49c0ae9fd4f3dda22c5b
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2023-11-10 20:28:21 +0700

    Add files via upload

---
+++ Lab_4_Loop/E8pi.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main() {
+
+    long long n=0;
+
+    double sign, pi, addend, sum=0;
+
+    do {
+        
+        // Calculate (-1)^n
+
+        if (n%2 == 0) sign = 1.0;
+
+        else sign = -1.0;
+
+        // Calculate each addend and sum
+
+        addend = sign / (2.0*n + 1.0);
+        sum += addend;
+        n++;
+    }
+    
+    while (abs(addend*4) >= 0.000001);
+
+    cout << "Pi = " << sum*4;
+
+    return 0;
+}
