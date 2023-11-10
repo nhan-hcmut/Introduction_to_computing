37696243ca7412e0a907c954bd95b055444d1a1b
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2023-11-10 20:40:43 +0700

    Add files via upload

---
+++ Lab_4_Loop/E9sinCosTan.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main() {
+    const double PI = 3.14159265359;
+
+    long long x; // Degree of angles
+
+    for (x=5; x<=85; x+=5) {
+        
+        cout << "Sin(" << x <<") = " << sin(x*PI/180) << " ; cos ("<< x << ") = " << cos(x*PI/180) << " ; tan (" << x << ") = " << tan(x*PI/180) << endl << endl;
+    }
+    return 0;
+}
