d18d9b49b43f30081fa47fc96e868f3f78f0c37f
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2023-11-10 21:24:25 +0700

    Add files via upload

---
+++ Lab_4_Loop/E12fiboArray.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+
+    int size;
+
+    cout << "How many Fibonacci numbers: ";
+    cin >> size;
+
+    int* F = new int[size+1];
+
+    F[0] = NULL; // F[i] will store the ith number in Fibonacci sequence => THERE IS NO F[0]
+
+    for (int i=1; i<=size; i++) {
+
+        if (i==1) F[i] = 0;
+
+        else if (i==2) F[i] = 1;
+
+        else F[i] = F[i-1] + F[i-2];
+    }
+
+    for (int i=1; i<=size; i++) {
+        cout << "F[" << i << "] = " << F[i] << endl;
+    }
+    
+    delete[] F;
+
+    return 0;
+}
