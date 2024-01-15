96859ae36c6476b13966a071bd39e7ea75d49355
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-01-15 23:24:25 +0700

    Update E6quadraticEquation.cpp

--- Lab_1_flowchart/E6quadraticEquation.cpp
+++ Lab_1_flowchart/E6quadraticEquation.cpp
@@ -15,7 +15,18 @@ int main() {
 
     delta = (b*b) - (4*a*c);
 
-    if (delta < 0) {
+    if (a == 0) { // If a is zero, we have a linear equation.
+        if (b == 0) {
+            if (c == 0) cout << "Infinite solutions.\n";
+            else cout << "No solution.\n";
+        }
+        else {
+            if (c == 0) cout << "Root = 0\n"; // Remove the output "Root = -0"
+            else cout << "Root = " << -c/b << endl;
+        }
+    }
+    /* Start solving quadratic equation from here */
+    else if (delta < 0) {
         cout << "There are no real roots.";
     }
     else if (delta == 0) {
