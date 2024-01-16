74583aaa8691407d78fd4dc649b5833e1762e197
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-01-16 22:26:56 +0700

    Update E11oddEven.cpp

--- Lab_2_InputOutput/E11oddEven.cpp
+++ Lab_2_InputOutput/E11oddEven.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main() {
 
-    int num;
+    int num = 0;
 
     cout << "Enter an integer: ";
     cin >> num;
 
-    (num%2 != 0) ? cout << "Odd" : cout << "Even"; // I must NOT use if statement
+    (num%2 != 0) ? cout << "Odd\n" : cout << "Even\n"; // I am NOT allowed to use if statement
 
     return 0;
 }
