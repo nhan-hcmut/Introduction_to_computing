a1f8f70383d5aefa7a10c20ba2de7c0f73138701
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-01-16 22:16:27 +0700

    Update E11leapYear.cpp

--- Lab_1_flowchart/E11leapYear.cpp
+++ Lab_1_flowchart/E11leapYear.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main() {
 
-    int year;
+    int year = 0;
 
     cout << "Enter a year: ";
     cin >> year;
 
-    if ( ( year%400 == 0 ) || ( year%4 == 0 && year%100 != 0 ) ) cout << "Leap year";
+    if ( ( year%400 == 0 ) || ( year%4 == 0 && year%100 != 0 ) ) cout << "Leap year.\n";
 
-    else cout << "Not a leap year";
+    else cout << "Not a leap year.\n";
     
     return 0;
 }
