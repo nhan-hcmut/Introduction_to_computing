d06b42fd24f0c773be0f5741d5a0cd6debe46eeb
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-01-16 22:22:11 +0700

    Update E6circumference.cpp

--- Lab_2_InputOutput/E6circumference.cpp
+++ Lab_2_InputOutput/E6circumference.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main() {
 
     const double PI = 3.14159265;
-    double r;
+    double r = 0;
     
     cout << "Radius: ";
     cin >> r;
 
-    cout << "Circumference: " << 2*PI*r;
+    cout << "Circumference: " << 2*PI*r << endl;
     
     return 0;
 }
