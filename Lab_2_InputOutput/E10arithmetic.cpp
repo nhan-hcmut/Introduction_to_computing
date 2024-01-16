a1ee71e615e83b84b039a0d01b161a572732ab35
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-01-16 21:42:04 +0700

    Update E10arithmetic.cpp

--- Lab_2_InputOutput/E10arithmetic.cpp
+++ Lab_2_InputOutput/E10arithmetic.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main() {
 
-    int num1, num2, addition, subtraction, multiplication;
-    float division;
+    int num1 = 0, num2 = 0, addition = 0, subtraction = 0, multiplication = 0, exponentiation = 1;
+    float division = 0;
 
     cout << "The first integer: ";
     cin >> num1;
@@ -16,11 +16,17 @@ int main() {
     subtraction = num1-num2;
     multiplication = num1*num2;
     division = (float)num1 / (float)num2;
+    int i = 1;
+    while (i <= num2) {
+        exponentiation *= num1;
+        i++;
+    }
 
     cout << "Sum: " << addition << endl;
     cout << "Difference: " << subtraction << endl;
     cout << "Product: " << multiplication << endl;
     cout << "Quotient: " << division << endl;
+    cout << num1 << " raised to the power of " << num2 << ": " << exponentiation << endl;
     
     return 0;
 }
