371de3159ea35f928652375591ec26fa04371373
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-01-16 22:14:01 +0700

    Update E7average.cpp

--- Lab_1_flowchart/E7average.cpp
+++ Lab_1_flowchart/E7average.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main() {
 
-    int n, i = 1;
-    double sum = 0, num, average;
+    int n = 0, i = 1;
+    double sum = 0, num = 0, average = 0;
 
     cout << "How many numbers: ";
     cin >> n;
