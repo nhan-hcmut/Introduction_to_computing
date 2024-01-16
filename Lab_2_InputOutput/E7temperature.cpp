5785f1a0bc93a9863881335116f3138887203ff3
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-01-16 22:22:49 +0700

    Update E7temperature.cpp

--- Lab_2_InputOutput/E7temperature.cpp
+++ Lab_2_InputOutput/E7temperature.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main() {
 
-    float fahrenheit, celsius;
+    float fahrenheit = 0, celsius = 0;
 
     cout << "Fahrenheit: ";
     cin >> fahrenheit;
 
     celsius = (fahrenheit-32) / 1.8;
 
-    cout << "Celsius: " << celsius;
+    cout << "Celsius: " << celsius << endl;
 
     return 0;
 }
