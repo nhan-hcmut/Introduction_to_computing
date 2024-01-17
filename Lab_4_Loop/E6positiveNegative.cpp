958228d9ecfab8f575a7825e2dbf854c22855224
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-01-17 09:53:27 +0700

    Update E6positiveNegative.cpp

--- Lab_4_Loop/E6positiveNegative.cpp
+++ Lab_4_Loop/E6positiveNegative.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main() {
 
-    int n, positive=0, negative=0;
-    float num;
-    bool zero=false; // There is/are the number(s) zero(s) or not
+    int n = 0, positive = 0, negative = 0;
+    float num = 0;
+    bool zero = false; // There is/are the number(s) zero(s) or not
 
     cout << "How many real numbers: ";
     cin >> n;
@@ -19,12 +19,12 @@ int main() {
 
         else if (num > 0) positive++;
 
-        else zero=true;
+        else zero = true;
     }
     
     cout << "There are " << negative << " negative numbers and " << positive << " positive numbers.\n";
     
-    if (zero == true) cout << "There is/are number(s) zero(s).";
+    if (zero == true) cout << "There is/are number(s) zero(s).\n";
     
     return 0;
 }
