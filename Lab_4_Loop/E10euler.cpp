f47a4ed9d7aeb8d6a0af4d409e3bc252eebbc622
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-01-17 09:49:54 +0700

    Update E10euler.cpp

--- Lab_4_Loop/E10euler.cpp
+++ Lab_4_Loop/E10euler.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 int main() {
 
-    long long n=1, factorial;
-    double e=1, addend;
+    long long n = 1, factorial = 0;
+    double e = 1, addend = 0;
 
     do {
-        factorial=1;
+        factorial = 1;
 
         for (int i=1; i<=n; i++) {
             factorial *= i;
         }
 
-        addend = 1.0/factorial;
+        addend = 1.0 / factorial;
         e += addend;
         n++;
     }
 
     while (addend >= 0.000001);
     
-    cout << "Euler's number = " << e;
+    cout << "Euler's number = " << e << endl;
 
     return 0;
 }
