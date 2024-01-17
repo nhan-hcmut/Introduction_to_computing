26d87b697eba6a915f71a2b116f067b64666c846
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-01-17 09:48:49 +0700

    Update E11fibonacci.cpp

--- Lab_4_Loop/E11fibonacci.cpp
+++ Lab_4_Loop/E11fibonacci.cpp
@@ -3,25 +3,25 @@ using namespace std;
 
 int main() {
 
-    int fn /* Fib n */, fn_2=0 /* Fib n-2 */, fn_1=1 /* Fib n-1 */, n, i=3;
+    int fn = 0 /* Fib n */, fn_2 = 0 /* Fib n-2 */, fn_1 = 1 /* Fib n-1 */, n = 0, i = 3;
 
     cout << "nth number in Fibonacci sequence: ";
     cin >> n;
 
-    if (n==1) cout << "The first Fibonacci number is 0";
+    if (n == 1) cout << "The first Fibonacci number is 0\n";
 
-    else if (n==2) cout << "The second Fibonacci number is 1";
+    else if (n == 2) cout << "The second Fibonacci number is 1\n";
 
     else {
 
-        while (i<=n) {
+        while (i <= n) {
 
             fn = fn_1 + fn_2;
             fn_2 = fn_1;
             fn_1 = fn;
             i++;
         }
-        cout << "Fib " << n << ": " << fn;
+        cout << "Fib " << n << ": " << fn << endl;
     }
     return 0;
 }
