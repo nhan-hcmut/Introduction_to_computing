8b2bc04083014219a5eb753a3ddf7f48b10ba596
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-01-17 09:43:50 +0700

    Update E13wages.cpp

--- Lab_4_Loop/E13wages.cpp
+++ Lab_4_Loop/E13wages.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main () {
 
-    int i;
+    int i = 0;
 
-    double hourly_rates[5] = {9.5 , 6.4 , 12.5 , 5.5 , 10.5}, working_hours[5], wages[5];
+    double hourly_rates[5] = {9.5 , 6.4 , 12.5 , 5.5 , 10.5}, working_hours[5] = {0 , 0 , 0 , 0 , 0}, wages[5] = {0 , 0 , 0 , 0 , 0};
 
     for (i=0; i<5; i++) {
 
