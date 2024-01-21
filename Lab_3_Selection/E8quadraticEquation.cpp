74f7f1d6f06f0d8cf541db9ef0c9ac1ca02ce344
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-01-21 12:22:08 +0700

    Update E8quadraticEquation.cpp

--- Lab_3_Selection/E8quadraticEquation.cpp
+++ Lab_3_Selection/E8quadraticEquation.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
 
-    float a, b, c, delta;
+    float a = 0, b = 0, c = 0, delta = 0;
 
     cout << "a: ";
     cin >> a;
@@ -17,13 +17,13 @@ int main() {
     delta = (b*b) - (4*a*c);
 
     if (delta < 0) {
-        cout << "There are no real roots";
+        cout << "There are no real roots\n";
     }
     else if (delta == 0) {
-        cout << "There is only one root";
+        cout << "There is only one root\n";
     }
     else {
-        cout << "There are two real roots";
+        cout << "There are two real roots\n";
     }
 
     return 0;
