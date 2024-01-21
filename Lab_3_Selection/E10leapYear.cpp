63aa3da4b1fc484a26815906b069705d9c13dd57
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-01-21 12:13:58 +0700

    Update E10leapYear.cpp

--- Lab_3_Selection/E10leapYear.cpp
+++ Lab_3_Selection/E10leapYear.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 int main() {
 
-    int year;
+    int year = 0;
 
     cout << "Year: ";
     cin >> year;
     
     if (year%400 == 0) {
-        cout << "Leap year";
+        cout << "Leap year\n";
     }
     else if ( (year%4 == 0) && (year%100 != 0) ) {
-        cout << "Leap year";
+        cout << "Leap year\n";
     }
     else {
-        cout << "Not leap year";
+        cout << "Not leap year\n";
     }
 
     return 0;
