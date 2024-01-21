4c8cb7ddb2dfe215a39cb7bc9b764355b3792c15
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-01-21 12:15:12 +0700

    Update E12positiveNegative.cpp

--- Lab_3_Selection/E12positiveNegative.cpp
+++ Lab_3_Selection/E12positiveNegative.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 int main() {
 
-    int num;
+    int num = 0;
     
     cout << "Enter a number: ";
     cin >> num;
 
     if (num > 0) {
-        cout << "Positive";
+        cout << "Positive\n";
     }
     else if (num < 0) {
-        cout << "Negative";
+        cout << "Negative\n";
     }
     else {
-        cout << "Zero";
+        cout << "Zero\n";
     }
 
     return 0;
