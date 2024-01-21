c55da893a40093dda0352537d5a2681a13683500
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-01-21 12:18:22 +0700

    Update E4daysInMonth.cpp

--- Lab_3_Selection/E4daysInMonth.cpp
+++ Lab_3_Selection/E4daysInMonth.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 int main() {
 
-    int month;
+    int month = 0;
 
     cout << "Month: ";
     cin >> month;
 
     if (month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12) {
-        cout << "31 days";
+        cout << "31 days\n";
     }
     else if (month==4 || month==6 || month==9 || month==11) {
-        cout<<"30 days.";
+        cout<<"30 days\n";
     }
     else {
-        cout << "28 (not leap years) or 29 days (leap years).";
+        cout << "28 (not leap years) or 29 days (leap years)\n";
     }
 
     return 0;
