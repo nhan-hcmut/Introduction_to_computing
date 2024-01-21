905c65a27ca52671cb7cf99abc0ee913d57974e5
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-01-21 12:17:23 +0700

    Update E3angle.cpp

--- Lab_3_Selection/E3angle.cpp
+++ Lab_3_Selection/E3angle.cpp
@@ -3,25 +3,25 @@ using namespace std;
 
 int main() {
 
-    int degree;
+    int degree = 0;
 
     cout << "Enter an angle (expressed in degrees): ";
     cin >> degree;
 
     if (0<=degree && degree<90) {
-        cout << "First quadrant";
+        cout << "First quadrant\n";
     }
     else if (90<=degree && degree<180) {
-        cout<<"Second quadrant";
+        cout<<"Second quadrant\n";
     }
     else if (180<=degree && degree<270) {
-        cout<<"Third quadrant";
+        cout<<"Third quadrant\n";
     }
     else if (270<=degree && degree<360) {
-        cout<<"Fourth quadrant";
+        cout<<"Fourth quadrant\n";
     }
     else {
-        cout << "Please run this program again and enter another number.";
+        cout << "Please run this program again and enter another number.\n";
     }
 
     return 0;
