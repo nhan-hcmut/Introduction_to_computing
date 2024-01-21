f0cb0627879801e433a1d878a17803e4b6e4eb62
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-01-21 12:20:09 +0700

    Update E6marriage.cpp

--- Lab_3_Selection/E6marriage.cpp
+++ Lab_3_Selection/E6marriage.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main () {
 
-    char code;
+    char code = '\0';
 
     cout << "Enter marriage code: ";
     cin >> code;
@@ -11,24 +11,24 @@ int main () {
     switch (code) {
 
     case 'M' :
-    cout << "Individual is married"; break;
+    cout << "Individual is married\n"; break;
 
     case 'm' :
-    cout << "Individual is married"; break;
+    cout << "Individual is married\n"; break;
 
     case 'D' :
-    cout << "Individual is divorced"; break;
+    cout << "Individual is divorced\n"; break;
 
     case 'd' :
-    cout << "Individual is divorced"; break;
+    cout << "Individual is divorced\n"; break;
 
     case 'W' :
-    cout << "Individual is widowed"; break;
+    cout << "Individual is widowed\n"; break;
 
     case 'w' :
-    cout << "Individual is widowed"; break;
+    cout << "Individual is widowed\n"; break;
 
-    default : cout << "An invalid code was entered";
+    default : cout << "An invalid code was entered\n";
     }
     
     return 0;
