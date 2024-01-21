8dcfa4252639ff5ba94091a22a60b2e8c1ac0ff3
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-01-21 12:23:49 +0700

    Update E13yearsWeeksDays.cpp

--- Lab_2_InputOutput/E13yearsWeeksDays.cpp
+++ Lab_2_InputOutput/E13yearsWeeksDays.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
 
-    int num, year, week, day;
+    int num = 0, year = 0, week = 0, day = 0;
 
     cout << "Enter number of days: ";
     cin >> num;
@@ -12,7 +12,7 @@ int main() {
     week = (num - year*365) / 7;
     day = num - (year*365) - (week*7);
 
-    cout << num << " days = " << year << " years + " << week << " weeks + " << day << " days.";
+    cout << num << " days = " << year << " years + " << week << " weeks + " << day << " days.\n";
     
     return 0;
 }
