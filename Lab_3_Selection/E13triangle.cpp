5977c0af2c536f632469f805011b81a8e9efe081
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-01-21 12:15:58 +0700

    Update E13triangle.cpp

--- Lab_3_Selection/E13triangle.cpp
+++ Lab_3_Selection/E13triangle.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
 
-    float edge1, edge2, edge3;
+    float edge1 = 0, edge2 = 0, edge3 = 0;
 
     cout << "First edge: ";
     cin >> edge1;
@@ -15,13 +15,13 @@ int main() {
     cin >> edge3;
     
     if (edge1 == edge2 && edge2 == edge3) {
-        cout << "Equilateral";
+        cout << "Equilateral\n";
     }
     else if (edge1 == edge2 || edge2 == edge3 || edge3 == edge1) {
-        cout << "Isosceles";
+        cout << "Isosceles\n";
     }
     else {
-        cout << "Scalene";
+        cout << "Scalene\n";
     }
     return 0;
 }
