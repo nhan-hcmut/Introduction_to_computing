8a47e6e64187d800655305c0ba60d65ed69a840e
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-01-24 08:46:12 +0700

    Update E4smallestNumber.cpp

--- Lab_1_flowchart/E4smallestNumber.cpp
+++ Lab_1_flowchart/E4smallestNumber.cpp
@@ -14,12 +14,12 @@ int main() {
     cout << "The third number: ";
     cin >> third_num;
 
-    if (first_num < second_num) min = first_num;
+    if (first_num <= second_num) min = first_num;
     else min = second_num;
 
     if (third_num < min) min = third_num;
     
-    cout << "The smallest number: " << min;
+    cout << "The smallest number: " << min << endl;
 
     return 0;
 }
