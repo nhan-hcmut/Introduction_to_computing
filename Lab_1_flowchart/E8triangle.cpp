805210895bd9b63837e857521fafb329d5bed5b8
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-01-27 20:28:59 +0700

    Update E8triangle.cpp

--- Lab_1_flowchart/E8triangle.cpp
+++ Lab_1_flowchart/E8triangle.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
+#include<cmath>
+
+#define EXP 1e-6
+
 using namespace std;
 
 int main() {
 
-    float edge1 = 0, edge2 = 0, edge3 = 0;
+    double edge1 = 0, edge2 = 0, edge3 = 0;
 
     cout << "The first edge: ";
     cin >> edge1;
@@ -18,17 +22,17 @@ int main() {
         cout << "Not a triangle.\n";
     }
 
-    else if (edge1==edge2 && edge2==edge3) {
+    else if (edge1 == edge2 && edge2 == edge3) {
         cout << "Equilateral\n";
     }
-    else if (edge1==edge2 || edge2==edge3 || edge3==edge1) {
+    else if (edge1 == edge2 || edge2 == edge3 || edge3 == edge1) {
 
-        if ((edge1*edge1) == (edge2*edge2 + edge3*edge3) || (edge2*edge2) == (edge1*edge1 + edge3*edge3) || (edge3*edge3) == (edge1*edge1 + edge2*edge2)) cout << "Isosceles right-angled\n";
+        if ( abs((edge1*edge1) - (edge2*edge2 + edge3*edge3)) < EXP || abs((edge2*edge2) - (edge1*edge1 + edge3*edge3)) < EXP || abs((edge3*edge3) - (edge1*edge1 + edge2*edge2)) < EXP) cout << "Isosceles right-angled\n";
 
         else cout << "Isosceles\n";
     }
 
-    else if ((edge1*edge1) == (edge2*edge2 + edge3*edge3) || (edge2*edge2) == (edge1*edge1 + edge3*edge3) || (edge3*edge3) == (edge1*edge1 + edge2*edge2)) cout << "Right-angled\n";
+    else if ( abs((edge1*edge1) - (edge2*edge2 + edge3*edge3)) < EXP || abs((edge2*edge2) - (edge1*edge1 + edge3*edge3)) < EXP || abs((edge3*edge3) - (edge1*edge1 + edge2*edge2)) < EXP) cout << "Right-angled\n";
 
     else cout << "Scalene\n";
     
