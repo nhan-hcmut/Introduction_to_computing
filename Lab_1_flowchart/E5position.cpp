5e74de2ca9ed6da11c39710779d35bce53673da7
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-06-25 11:13:19 +0700

    Update E5position.cpp

--- Lab_1_flowchart/E5position.cpp
+++ Lab_1_flowchart/E5position.cpp
@@ -4,29 +4,31 @@ using namespace std;
 
 int main() {
 
-    float r = 0, distance = 0, x0 = 0, y0 = 0, x = 0, y = 0; // Center O (x0, y0), point A (x, y)
+    float radius = 0, distance = 0, xO = 0, yO = 0, x = 0, y = 0; // Center O (xO, yO), point A (x, y)
 
     cout << "Point A" << endl;
     cout << "x: ";
     cin >> x;
+    
     cout << "y: ";
     cin >> y;
 
     cout << "Center O" << endl;
-    cout << "x0: ";
+    cout << "xO: ";
     cin >> x0;
-    cout << "y0: ";
+    
+    cout << "yO: ";
     cin >> y0;
 
     cout << "Radius of the circle: ";
-    cin >> r;
+    cin >> radius;
 
-    distance = sqrt( (x-x0) * (x-x0) + (y-y0) * (y-y0) );
+    distance = sqrt( (x-xO) * (x-xO) + (y-yO) * (y-yO) );
 
-    if (distance == r) {
+    if (distance == radius) {
         cout << "Point A is on the circle.\n";
     }
-    else if (distance < r) {
+    else if (distance < radius) {
         cout << "Point A is inside the circle.\n";
     }
     else cout << "Point A is outside the circle.\n";
